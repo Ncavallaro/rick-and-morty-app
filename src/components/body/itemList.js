2218b9aa7d547583f750d3ebf88d779254148483
Ncavallaro <nicolascavallaro@live.com>
2023-11-17 12:41:18 +0100

    Fix css

--- src/components/body/itemList.js
+++ src/components/body/itemList.js
@@ -1,18 +1,18 @@
 import '../../css/body/itemList.css'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 const itemList = (props) => {
   return (
-    <div className="container" id="containerList">
-        <div className="row" id="rowList">
+    <div className='container' id='containerList'>
+        <div className='row' id='rowList'>
           {props.characters.map(character => (
             <Link to={`/character/${character.id}`} className='col-lg-4 col-md-6 col-sm-12' id='Link' key={character.id}>
               <div key={character.id} >
-                <div className="card mb-3">
-                  <img src={character.image} className="card-img-top" alt={character.name} />
-                  <div className="card-body">
-                    <h3 className="card-title">{character.name}</h3>
-                    <p className="card-text">
+                <div className='card mb-3'>
+                  <img src={character.image} className='card-img-top' alt={character.name} />
+                  <div className='card-body'>
+                    <h3 className='card-title'>{character.name}</h3>
+                    <p className='card-text'>
                       <strong>Location:</strong> {character.location.name}
                       <br />
                       <strong>Origin:</strong> {character.origin.name}
@@ -29,4 +29,4 @@ const itemList = (props) => {
   )
 }
 
-export default itemList;
+export default itemList
--- src/components/body/itemListContainer.js
+++ src/components/body/itemListContainer.js
@@ -1,60 +1,68 @@
-import React, { useState } from 'react';
-import '../../css/body/itemListContainer.css';
-import ItemList from '../body/itemList';
-import Pagination from '../body/pagination';
-import Search from '../body/search';
-import useFetchCharacters from '../../api/apiListCharacters';
+import React, { useState } from 'react'
+import '../../css/body/itemListContainer.css'
+import ItemList from '../body/itemList'
+import Pagination from '../body/pagination'
+import Search from '../body/search'
+import useFetchCharacters from '../../api/apiListCharacters'
 
 const ItemListContainer = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [locationFilter, setLocationFilter] = useState('');
-  const [nameFilter, setNameFilter] = useState('');
+  const [currentPage, setCurrentPage] = useState(1)
+  const [locationFilter, setLocationFilter] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
-  const { characters, totalPages } = useFetchCharacters(currentPage, locationFilter, nameFilter);
+  const { characters, totalPages } = useFetchCharacters(
+    currentPage,
+    locationFilter,
+    nameFilter
+  )
 
   const resetFilters = () => {
-    setNameFilter('');
-    setLocationFilter('');
-  };
+    setNameFilter('')
+    setLocationFilter('')
+  }
 
   const handleNextPage = () => {
-    resetFilters();
-    setCurrentPage(currentPage + 1);
-  };
+    resetFilters()
+    setCurrentPage(currentPage + 1)
+  }
 
   const handlePreviousPage = () => {
     if (currentPage > 1) {
-      resetFilters();
-      setCurrentPage(currentPage - 1);
+      resetFilters()
+      setCurrentPage(currentPage - 1)
+      console.log(totalPages)
     }
-  };
+  }
 
   const handleLocationFilterChange = (event) => {
-    event.preventDefault();
-    setCurrentPage(1);
-    setLocationFilter(event.target.previousSibling.value);
+    event.preventDefault()
+    setCurrentPage(1)
+    setLocationFilter(event.target.previousSibling.value)
   };
 
   const handleNameFilterChange = (event) => {
-    event.preventDefault();
-    setCurrentPage(1);
-    setNameFilter(event.target.previousSibling.value);
-  };
+    event.preventDefault()
+    setCurrentPage(1)
+    setNameFilter(event.target.previousSibling.value)
+  }
 
   return (
     <>
-      <div id="contanierBody">
-        <div className="container-fluid">
+      <div id='contanierBody'>
+        <div className='container-fluid' id='containerSearches'>
           <Search
             handleNameFilterChange={handleNameFilterChange}
             handleLocationFilterChange={handleLocationFilterChange}
           />
         </div>
         <ItemList characters={characters} />
-        <Pagination handleNextPage={handleNextPage} handlePreviousPage={handlePreviousPage} />
+        <Pagination
+          handleNextPage={handleNextPage}
+          handlePreviousPage={handlePreviousPage}
+        />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ItemListContainer;
+export default ItemListContainer
